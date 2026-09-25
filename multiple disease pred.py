f53a4e65b473199ae162cbf827a12fa72e082ea5
Divyanjali <2k23.csds2313690@gmail.com>
2026-09-26 03:59:40 +0530

    Update multiple disease pred.py

--- multiple disease pred.py
+++ multiple disease pred.py
@@ -39,48 +39,59 @@ with st.sidebar:
 # Diabetes prediction page
 
 if selected == 'Diabetes Prediction':
-    
-    #page title
+
     st.title('Diabetes Prediction using ML')
-    
-    
-    #getting the input data from the user
-    #columns for input fields
+
     col1, col2, col3 = st.columns(3)
-    
+
     with col1:
         pregnancies = st.text_input('Number of Pregnancies')
-        
+
     with col2:
         Glucose = st.text_input('Glucose Level')
+
     with col3:
         BloodPressure = st.text_input('Blood Pressure value')
+
     with col1:
         SkinThickness = st.text_input('Skin Thickness value')
+
     with col2:
         Insulin = st.text_input('Insulin Level')
+
     with col3:
         BMI = st.text_input('BMI value')
+
     with col1:
-        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+        DiabetesPedigreeFunction = st.text_input(
+            'Diabetes Pedigree Function value'
+        )
+
     with col2:
         Age = st.text_input('Age of the Person')
 
-    
-    
-    #code for prediction
     diab_diagnosis = ''
-    
-    #creating a button for prediction
-    
-    if st.button('diabetes Test Result'):
-        diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age ]])
-        
-        if (diab_prediction[0]==1):
+
+    if st.button('Diabetes Test Result'):
+
+        diab_prediction = diabetes_model.predict([[
+            float(pregnancies),
+            float(Glucose),
+            float(BloodPressure),
+            float(SkinThickness),
+            float(Insulin),
+            float(BMI),
+            float(DiabetesPedigreeFunction),
+            float(Age)
+        ]])
+
+        if diab_prediction[0] == 1:
             diab_diagnosis = 'The person is Diabetic'
         else:
             diab_diagnosis = 'The person is not Diabetic'
-    st.success(diab_diagnosis)        
+
+    if diab_diagnosis:
+        st.success(diab_diagnosis) 
     
  
 
